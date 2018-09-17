937038f1eba37470c9986d2879004968ab1132f2
XiaTerry <1363314380@qq.com>
2018-09-17 23:12:59 +0800

    react-my-app搭建-1

---
+++ my-react-app/src/App.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'antd-mobile';
+import { Carousel, WingBlank } from 'antd-mobile';
+
+class App2 extends React.Component {
+  state = {
+    data: ['1', '2', '3'],
+    imgHeight: 176,
+  }
+  componentDidMount() {
+    // simulate img loading
+    setTimeout(() => {
+      this.setState({
+        data: ['AiyWuByWklrrUDlFignR', 'TekJlZRVCjLFexlOCuWn', 'IJOtIlfsYdTyaDTRVrLI'],
+      });
+    }, 100);
+  }
+  render() {
+    return (
+      <WingBlank>
+        <Carousel className="space-carousel"
+          frameOverflow="visible"
+          cellSpacing={10}
+          slideWidth={0.8}
+          autoplay
+          infinite
+          beforeChange={(from, to) => console.log(`slide from ${from} to ${to}`)}
+          afterChange={index => this.setState({ slideIndex: index })}
+        >
+          {this.state.data.map((val, index) => (
+            <a
+              key={val}
+              href="http://www.alipay.com"
+              style={{
+                display: 'block',
+                position: 'relative',
+                top: this.state.slideIndex === index ? -10 : 0,
+                height: this.state.imgHeight,
+                boxShadow: '2px 1px 1px rgba(0, 0, 0, 0.2)',
+              }}
+            >
+              <img
+                src={`https://zos.alipayobjects.com/rmsportal/${val}.png`}
+                alt=""
+                style={{ width: '100%', verticalAlign: 'top' }}
+                onLoad={() => {
+                  // fire window resize event to change height
+                  window.dispatchEvent(new Event('resize'));
+                  this.setState({ imgHeight: 'auto' });
+                }}
+              />
+            </a>
+          ))}
+        </Carousel>
+      </WingBlank>
+    );
+  }
+}
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        {/* <App2/> */}
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Button>你好</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
