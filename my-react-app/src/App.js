07fc81bf395bc2d561f70ad42b16124740f9f280
XiaTerry <1363314380@qq.com>
2018-09-18 21:33:16 +0800

    react-app头部和底部

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,76 +1,133 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Button } from 'antd-mobile';
-import { Carousel, WingBlank } from 'antd-mobile';
+import '@/App.css';
+import { NavBar, Icon, TabBar} from 'antd-mobile';
 
-class App2 extends React.Component {
-  state = {
-    data: ['1', '2', '3'],
-    imgHeight: 176,
-  }
-  componentDidMount() {
-    // simulate img loading
-    setTimeout(() => {
-      this.setState({
-        data: ['AiyWuByWklrrUDlFignR', 'TekJlZRVCjLFexlOCuWn', 'IJOtIlfsYdTyaDTRVrLI'],
-      });
-    }, 100);
-  }
-  render() {
-    return (
-      <WingBlank>
-        <Carousel className="space-carousel"
-          frameOverflow="visible"
-          cellSpacing={10}
-          slideWidth={0.8}
-          autoplay
-          infinite
-          beforeChange={(from, to) => console.log(`slide from ${from} to ${to}`)}
-          afterChange={index => this.setState({ slideIndex: index })}
-        >
-          {this.state.data.map((val, index) => (
-            <a
-              key={val}
-              href="http://www.alipay.com"
-              style={{
-                display: 'block',
-                position: 'relative',
-                top: this.state.slideIndex === index ? -10 : 0,
-                height: this.state.imgHeight,
-                boxShadow: '2px 1px 1px rgba(0, 0, 0, 0.2)',
-              }}
-            >
-              <img
-                src={`https://zos.alipayobjects.com/rmsportal/${val}.png`}
-                alt=""
-                style={{ width: '100%', verticalAlign: 'top' }}
-                onLoad={() => {
-                  // fire window resize event to change height
-                  window.dispatchEvent(new Event('resize'));
-                  this.setState({ imgHeight: 'auto' });
-                }}
-              />
-            </a>
-          ))}
-        </Carousel>
-      </WingBlank>
-    );
-  }
-}
+
+
 class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      hidden:false,
+      fullScreen:true,
+    }
+  }
   render() {
     return (
+      // 头部导航条部分
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        {/* <App2/> */}
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button>你好</Button>
+        <NavBar
+          mode="dark"
+          icon={<Icon type="left" />}
+          onLeftClick={() => console.log('onLeftClick')}
+        >黑马程序react</NavBar>
+        {/* 底部导航 */}
+        <div style={this.state.fullScreen ? { position: 'fixed', width: '100%', bottom: 0, marginTop:'45px'} : { height: 400 }}>
+        <TabBar
+          unselectedTintColor="#fff"
+          tintColor="#fff"
+          barTintColor="#108ee9"
+          hidden={this.state.hidden}
+          tabBarPosition="bottom"
+        >
+          <TabBar.Item
+            title="首页"
+            key="Life"
+            icon={<div style={{
+              width: '22px',
+              height: '22px',
+              background: 'url(https://zos.alipayobjects.com/rmsportal/sifuoDUQdAFKAVcFGROC.svg) center center /  21px 21px no-repeat' }}
+            />
+            }
+            selectedIcon={<div style={{
+              width: '22px',
+              height: '22px',
+              background: 'url(https://zos.alipayobjects.com/rmsportal/iSrlOTqrKddqbOmlvUfq.svg) center center /  21px 21px no-repeat' }}
+            />
+            }
+            selected={this.state.selectedTab === 'blueTab'}
+            // badge={1}
+            // selected={true}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'blueTab',
+              });
+            }}
+            data-seed="logId"
+          >
+            首页
+          </TabBar.Item>
+          <TabBar.Item
+            icon={
+              <div style={{
+                width: '22px',
+                height: '22px',
+                background: 'url(https://gw.alipayobjects.com/zos/rmsportal/BTSsmHkPsQSPTktcXyTV.svg) center center /  21px 21px no-repeat' }}
+              />
+            }
+            selectedIcon={
+              <div style={{
+                width: '22px',
+                height: '22px',
+                background: 'url(https://gw.alipayobjects.com/zos/rmsportal/ekLecvKBnRazVLXbWOnE.svg) center center /  21px 21px no-repeat' }}
+              />
+            }
+            title="会员"
+            key="Koubei"
+            // badge={'new'}
+            selected={this.state.selectedTab === 'redTab'}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'redTab',
+              });
+            }}
+            data-seed="logId1"
+          >
+           会员
+          </TabBar.Item>
+          <TabBar.Item
+            icon={
+              <div style={{
+                width: '22px',
+                height: '22px',
+                background: 'url(https://zos.alipayobjects.com/rmsportal/psUFoAMjkCcjqtUCNPxB.svg) center center /  21px 21px no-repeat' }}
+              />
+            }
+            selectedIcon={
+              <div style={{
+                width: '22px',
+                height: '22px',
+                background: 'url(https://zos.alipayobjects.com/rmsportal/IIRLrXXrFAhXVdhMWgUI.svg) center center /  21px 21px no-repeat' }}
+              />
+            }
+            title="购物车"
+            key="Friend"
+            // dot
+            selected={this.state.selectedTab === 'greenTab'}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'greenTab',
+              });
+            }}
+          >
+            购物车
+          </TabBar.Item>
+          <TabBar.Item
+            icon={{ uri: 'https://zos.alipayobjects.com/rmsportal/asJMfBrNqpMMlVpeInPQ.svg' }}
+            selectedIcon={{ uri: 'https://zos.alipayobjects.com/rmsportal/gjpzzcrPMkhfEqgbYvmN.svg' }}
+            title="我的"
+            key="my"
+            selected={this.state.selectedTab === 'yellowTab'}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'yellowTab',
+              });
+            }}
+          >
+            我的
+          </TabBar.Item>
+        </TabBar>
+        </div>
       </div>
     );
   }
