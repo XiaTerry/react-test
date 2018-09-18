7eae404405690ab295ad692537cb456a6a4e0186
XiaTerry <1363314380@qq.com>
2018-09-18 23:14:33 +0800

    抽屉菜单

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
 import '@/App.css';
-import { NavBar, Icon, TabBar} from 'antd-mobile';
-
+import { NavBar, Icon, TabBar,Drawer, List} from 'antd-mobile';
 
 
 class App extends Component {
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
     this.state={
       hidden:false,
       fullScreen:true,
+      open:true,
     }
+    this.onOpenChange = this.onOpenChange.bind(this);
   }
+  onOpenChange = (...args)=>{
+      console.log(args);
+      this.setState({ open: !this.state.open });
+    }
   render() {
+    const sidebar = (<List>
+      {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map((i, index) => {
+        if (index === 0) {
+          return (<List.Item key={index}
+            thumb="https://zos.alipayobjects.com/rmsportal/eOZidTabPoEbPeU.png"
+            multipleLine
+          >Category</List.Item>);
+        }
+        return (<List.Item key={index}
+          thumb="https://zos.alipayobjects.com/rmsportal/eOZidTabPoEbPeU.png"
+        >Category{index}</List.Item>);
+      })}
+    </List>);
     return (
       // 头部导航条部分
       <div className="App">
@@ -22,6 +40,7 @@ class App extends Component {
           onLeftClick={() => console.log('onLeftClick')}
         >黑马程序react</NavBar>
         {/* 底部导航 */}
+    
         <div style={this.state.fullScreen ? { position: 'fixed', width: '100%', bottom: 0, marginTop:'45px'} : { height: 400 }}>
         <TabBar
           unselectedTintColor="#fff"
@@ -55,8 +74,22 @@ class App extends Component {
             }}
             data-seed="logId"
           >
-            首页
+          
+          <NavBar icon={<Icon type="ellipsis" />} onLeftClick={this.onOpenChange}>Basic</NavBar>
+          <Drawer
+            className="my-drawer"
+            style={{ minHeight: document.documentElement.clientHeight }}
+            enableDragHandle
+            contentStyle={{ color: '#A6A6A6', textAlign: 'center', paddingTop: 42 }}
+            sidebar={sidebar}
+            open={this.state.open}
+            onOpenChange={this.onOpenChange}
+          >
+            {/* Click upper-left corner */}
+          </Drawer>
+          首页
           </TabBar.Item>
+          
           <TabBar.Item
             icon={
               <div style={{
